e45d4f0a72aea99befc229653d09ac165f0340a8
Nero <286830682@qq.com>
2021-05-04 01:11:51 +0800

    speed up solution_nero.cc

--- main/weighted-bipartite-matching/solution_nero.cc
+++ main/weighted-bipartite-matching/solution_nero.cc
@@ -6,83 +6,72 @@
 #define CHECK(...) 1145141919810
 #endif
 
-template <int kN, typename T> struct CostFlow {
-  const T inf = std::numeric_limits<T>::max();
-  int s, t, head[kN], etot, prevv[kN], preve[kN], inq[kN], que[kN], qf, qe;
-  T dis[kN];
-  struct Edge {
-    int v, next;
-    T cap, cost;
-  } g[601000];
+template <typename T> inline void minify(T &a, T b) {
+  if (a > b)
+    a = b;
+}
+
+const int INF = 0x3f3f3f3f;
+
+template <int kN> struct KM {
+  int lv[kN], rv[kN], la[kN], ra[kN], left[kN], G[kN][kN], n, m;
+  bool expath(int u) {
+    lv[u] = 1;
+    for (int i = 0; i < m; i++)
+      if (!rv[i] && la[u] + ra[i] == G[u][i]) {
+        rv[i] = 1;
+        if (left[i] == -1 || expath(left[i]))
+          return left[i] = u, true;
+      }
+    return false;
+  }
   void init() {
-    memset(head, -1, sizeof(head));
-    etot = 0;
-  }
-  void add_edge(int u, int v, T cap, T cost) {
-    g[etot].v = v;
-    g[etot].cap = cap;
-    g[etot].cost = cost;
-    g[etot].next = head[u];
-    head[u] = etot++;
-    g[etot].v = u;
-    g[etot].cap = 0;
-    g[etot].cost = -cost;
-    g[etot].next = head[v];
-    head[v] = etot++;
-  }
-  void spfa(int source) {
     for (int i = 0; i < kN; i++)
-      dis[i] = inf;
-    dis[source] = 0;
-    qf = qe = 0;
-    que[qe++] = source;
-    while (qf != qe) {
-      int u = que[qf++];
-      inq[u] = 0;
-      if (qf == kN)
-        qf = 0;
-      for (int i = head[u]; i != -1; i = g[i].next) {
-        Edge &e = g[i];
-        if (e.cap && dis[e.v] > dis[u] + e.cost) {
-          dis[e.v] = dis[u] + e.cost;
-          prevv[e.v] = u;
-          preve[e.v] = i;
-          if (!inq[e.v]) {
-            que[qe++] = e.v;
-            if (qe == kN)
-              qe = 0;
-            inq[e.v] = 1;
-          }
-        }
+      for (int j = 0; j < kN; j++)
+        G[i][j] = -INF;
+  }
+  int km(int _n, int _m) {
+    n = _n;
+    m = _m;
+    memset(left, -1, sizeof(left));
+    for (int i = 0; i < n; i++) {
+      la[i] = -INF;
+      for (int j = 0; j < m; j++)
+        la[i] = std::max(la[i], G[i][j]);
+    }
+    for (int i = 0; i < m; i++)
+      ra[i] = 0;
+    for (int u = 0; u < n; u++) {
+      for (int i = 0; i < n; i++)
+        lv[i] = 0;
+      for (int i = 0; i < m; i++)
+        rv[i] = 0;
+      while (!expath(u)) {
+        int d = INF;
+        for (int i = 0; i < n; i++)
+          if (lv[i])
+            for (int j = 0; j < m; j++)
+              if (!rv[j])
+                d = std::min(d, la[i] + ra[j] - G[i][j]);
+        for (int i = 0; i < n; i++)
+          if (lv[i])
+            la[i] -= d, lv[i] = 0;
+        for (int i = 0; i < m; i++)
+          if (rv[i])
+            ra[i] += d, rv[i] = 0;
       }
     }
-  }
-  void mcmf(int _s, int _t, T &cost, T &flow) {
-    s = _s;
-    t = _t;
-    cost = flow = 0;
-    while (true) {
-      spfa(_s);
-      if (dis[t] == inf)
-        break;
-      T f = inf;
-
-      for (int u = t; u != s; u = prevv[u])
-        f = std::min(f, g[preve[u]].cap);
-      cost += f * dis[t];
-      flow += f;
-      for (int u = t; u != s; u = prevv[u])
-        g[preve[u]].cap -= f, g[preve[u] ^ 1].cap += f;
-    }
+    int ret = 0;
+    for (int i = 0; i < m; i++)
+      if (left[i] != -1)
+        ret += G[left[i]][i];
+    return ret;
   }
 };
 
-using Flow = CostFlow<1002, int>;
-const int _s = 1000;
-const int _t = 1001;
-
-Flow f;
 int n, m;
+
+KM<500> km;
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -91,28 +80,35 @@ int main() {
 
   while (reader >> n >> m) {
     const int N = std::max(n, m);
-    f.init();
     for (int i = 0; i < N; ++i) {
       for (int j = 0; j < N; ++j) {
         int w = 0;
         if (i < n && j < m) {
           reader >> w;
         }
-        f.add_edge(i, j + N, 1, -w);
+        km.G[i][j] = w;
       }
     }
-    for (int i = 0; i < N; ++i) {
-      f.add_edge(_s, i, 1, 0);
-      f.add_edge(i + N, _t, 1, 0);
+    int best = km.km(N, N);
+    std::vector<std::vector<int>> graph(N + N, std::vector<int>(N + N, INF));
+    for (int i = 0; i < graph.size(); ++i)
+      graph[i][i] = 0;
+    for (int u = 0; u < N; ++u) {
+      for (int v = 0; v < N; ++v) {
+        if (km.left[v] != u)
+          minify(graph[u][v + N], -km.G[u][v]);
+        else
+          minify(graph[v + N][u], km.G[u][v]);
+      }
     }
-    int cost, flow;
-    f.mcmf(_s, _t, cost, flow);
-    CHECK(flow == N);
+    for (int k = 0; k < graph.size(); ++k)
+      for (int i = 0; i < graph.size(); ++i)
+        for (int j = 0; j < graph.size(); ++j)
+          minify(graph[i][j], graph[i][k] + graph[k][j]);
     std::vector<std::vector<int>> result(n, std::vector<int>(m));
     for (int j = 0; j < m; ++j) {
-      f.spfa(j + N);
       for (int i = 0; i < n; ++i) {
-        result[i][j] = cost + f.dis[i];
+        result[i][j] = -best + graph[j + N][i];
       }
     }
     for (int i = 0; i < n; ++i) {
