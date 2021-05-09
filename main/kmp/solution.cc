4506aa6d2804c38cf278e6500e89e6c7d50cdc69
Xi Lin <zimpha@gmail.com>
2021-05-09 23:23:56 +0800

    remove dfs

--- main/kmp/solution.cc
+++ main/kmp/solution.cc
@@ -8,10 +8,9 @@ const int N = 1e6 + 10;
 
 using i64 = long long;
 
-std::vector<int> edges[N];
 char s[N], t[N];
 int ps[N], pt[N];
-int n, m;
+int n, m, ret[N];
 bool occ[N];
 
 void kmp(char *s, int n, int *pi) {
@@ -21,21 +20,6 @@ void kmp(char *s, int n, int *pi) {
       j = pi[j];
     }
     pi[i] = (s[j + 1] == s[i] ? ++j : j);
-  }
-}
-
-int stack[N], ret[N], size;
-
-void dfs(int u) {
-  if (occ[u + 1]) {
-    stack[size++] = u;
-  }
-  ret[u] = size ? m + stack[size - 1] : 0;
-  for (auto &v : edges[u]) {
-    dfs(v);
-  }
-  if (size && stack[size - 1] == u) {
-    --size;
   }
 }
 
@@ -70,17 +54,16 @@ int main() {
         border = ps[border];
       }
     }
-    edges[0].clear();
-    for (int i = 1; i <= n; ++i) {
-      edges[i].clear();
-      edges[ps[i]].push_back(i);
+    ret[0] = 0;
+    for (int i = 0; i < n; ++i) {
+      if (occ[i + 1]) ret[i] = m + i;
+      else ret[i] = i ? ret[ps[i]] : 0;
     }
-    size = 0;
-    dfs(0);
     i64 sum = 0;
     for (int i = 0; i < n; ++i) {
-      if (ret[i] == n)
+      if (ret[i] == n) {
         ret[i] = ps[n];
+      }
       ret[i] = std::max(ret[i], border);
       sum += ret[i] ^ i;
     }
