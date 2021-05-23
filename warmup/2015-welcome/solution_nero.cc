23707d491d1caf81e753ae90b6b926118e96071f
Nero <286830682@qq.com>
2021-05-23 16:27:52 +0800

    Add solution_nero.cc

---
+++ warmup/2015-welcome/solution_nero.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#ifdef ALGO
+#include "el_psy_congroo.hpp"
+#else
+#define DUMP(...) 1145141919810
+#define CHECK(...) (__VA_ARGS__)
+#endif
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::istream &reader = std::cin;
+
+  int n;
+  while (reader >> n) {
+    n = std::min(n, 2015);
+    for (int i = 0; i < n; ++i) {
+      printf("ccpC\n");
+    }
+  }
+}
