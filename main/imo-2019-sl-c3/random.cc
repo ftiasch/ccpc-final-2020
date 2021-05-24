c6e01fc25fa8fc627020326547cb839693dbb5c0
Xiaoxu Guo <ftiasch0@gmail.com>
2021-05-24 17:38:43 +0800

    fixed

--- main/imo-2019-sl-c3/random.cc
+++ main/imo-2019-sl-c3/random.cc
@@ -4,9 +4,15 @@ int main(int argc, char *argv[]) {
   registerGen(argc, argv, 1);
 
   int T = opt<int>("T");
-  const int n = opt<int>("n");
+  // const int n = opt<int>("n");
   const int q = opt<int>("q");
   while (T--) {
+    int n;
+    if (has_opt("n")) {
+      n = opt<int>("n");
+    } else {
+      n = rnd.next(1, opt<int>("maxn"));
+    }
     printf("%d %d\n", n, q);
     printf("%s\n", rnd.next(format("[01]{%d}", n)).c_str());
     for (int _ = 0; _ < q; ++_) {
