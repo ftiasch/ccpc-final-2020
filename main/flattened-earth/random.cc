9e1afca09e7d8b6cd7f4d4aa47cd8eacc1209061
Xiaoxu Guo <ftiasch0@gmail.com>
2021-05-24 17:05:26 +0800

    changed random

--- main/flattened-earth/random.cc
+++ main/flattened-earth/random.cc
@@ -17,8 +17,15 @@ int main(int argc, char *argv[]) {
     }
     printf("%d %d\n", n, m);
     for (int i = 0; i < m; ++i) {
-      int a = rnd.next(1, n);
-      int b = rnd.next(1, n);
+      int a, b;
+      if (has_opt("maxlen")) {
+        int length = rnd.next(0, opt<int>("maxlen"));
+        a = rnd.next(0, n - 1) + 1;
+        b = (a - 1 + length) % n + 1;
+      } else {
+        a = rnd.next(1, n);
+        b = rnd.next(1, n);
+      }
       printf("%d %d\n", a, b);
     }
   }
