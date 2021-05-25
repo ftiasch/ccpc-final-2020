56c69bd5724e73dd9c76b0415a66f98c3bc014ef
Sd-Invol <sd.invol@gmail.com>
2021-05-26 00:44:07 +0800

    Revert "Revert "Merge branch 'main' of https://github.com/ftiasch/ccpc-final-2020 into main""
    This reverts commit eef4dcd78e0138411787495b5a223f99f7cd81a8.

--- main/ones/solution_zimpha.cc
+++ main/ones/solution_zimpha.cc
@@ -1,19 +1,21 @@
+#include <algorithm>
+#include <cassert>
 #include <cstdio>
 #include <cstring>
-#include <cassert>
-#include <algorithm>
 #include <queue>
 
 using pii = std::pair<int, int>;
 
-const int N = 1e4 + 10, M = 1e6 + 10;
+const int N = 5000 + 10, M = 1e6 + 10;
+const int XM = N * 6;
+const int VM = XM + 1e5 + 10;
 
 char s[N];
 int _table[M * 2 + 1], pw[10];
-int* table = _table + M;
+int *table = _table + M;
 
-int dp[2][11][50001];
-int small_dp[2][M * 2 + 1];
+int dp[2][3][XM * 2 + 1];
+int small_dp[2][VM * 2 + 1];
 
 void prepare() {
   pw[0] = 1;
@@ -21,14 +23,16 @@ void prepare() {
     pw[i] = pw[i - 1] * 10;
   }
   std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
-  for (int i = -M; i <= M; ++i) table[i] = -1;
+  for (int i = -M; i <= M; ++i)
+    table[i] = -1;
   table[0] = 0;
   pq.emplace(0, 0);
   while (!pq.empty()) {
     int d = pq.top().first;
     int u = pq.top().second;
     pq.pop();
-    if (table[u] < d) continue;
+    if (table[u] < d)
+      continue;
     for (int e = 0; e < 2; ++e) {
       for (int i = 1; i <= 7; ++i) {
         int v = u + (pw[i] - 1) / 9 * (e == 0 ? 1 : -1);
@@ -41,19 +45,16 @@ void prepare() {
   }
 }
 
-int& at(int i, int carry, int sum) {
-  return dp[i & 1][carry + 5][sum + 25000];
-}
+int &at(int i, int carry, int sum) { return dp[i & 1][carry + 1][sum + XM]; }
 
-int& at(int i, int sum) {
-  return small_dp[i & 1][sum + M];
-}
+int &at(int i, int sum) { return small_dp[i & 1][sum + VM]; }
 
 void update(int &x, int y) {
-  if (x == -1 || x > y) x = y;
+  if (x == -1 || x > y)
+    x = y;
 }
 
-int main () {
+int main() {
   prepare();
   while (scanf("%s", s) == 1) {
     int n = strlen(s);
@@ -66,7 +67,8 @@ int main () {
     for (int i = 0, c = 0; i < n || (i >= n && c); ++i) {
       int o = c + (i < n ? (s[i] - '0') : 0) * 9;
       s[i] = '0' + o % 10;
-      if (i >= n) s[i + 1] = 0;
+      if (i >= n)
+        s[i + 1] = 0;
       c = o / 10;
     }
 
@@ -77,20 +79,20 @@ int main () {
     memset(dp, -1, sizeof(dp));
     at(n + 1, 0, 0) = 0;
     for (int i = n + 1; i > 5; --i) {
-      memset(dp[(i - 1) & 1], -1, sizeof(dp[0]));
       int o = (i - 1 < n) ? (s[i - 1] - '0') : 0;
       for (int carry = -carry_bound; carry <= carry_bound; ++carry) {
         for (int sum = -x_bound * n; sum <= n * x_bound; ++sum) {
-          int w = at(i, carry, sum);
-          if (w == -1) continue;
+          int &w = at(i, carry, sum);
+          if (w == -1)
+            continue;
           for (int x = -x_bound; x <= x_bound; ++x) {
             int new_carry = carry * 10 + x - o;
             int new_sum = x + sum;
-            //printf("i=%d carry=%d sum=%d new_carry=%d new_sum=%d\n", i, carry, sum, new_carry, new_sum);
             if (-carry_bound <= new_carry && new_carry <= carry_bound) {
               update(at(i - 1, new_carry, new_sum), w + std::abs(x) * (i - 1));
             }
           }
+          w = -1;
         }
       }
     }
@@ -98,21 +100,25 @@ int main () {
     for (int carry = -carry_bound; carry <= carry_bound; ++carry) {
       for (int sum = -x_bound * n; sum <= n * x_bound; ++sum) {
         int w = at(5, carry, sum);
-        if (w == -1) continue;
-        //printf("carry=%d sum=%d %d %d\n", carry, sum, carry * 100000 - sum, w);
+        if (w == -1)
+          continue;
         update(at(5, carry * 100000 - sum), w);
       }
     }
     int p10 = 10000;
     for (int i = 5; i >= 1; --i) {
       int o = s[i - 1] - '0';
-      memset(small_dp[(i - 1) & 1], -1, sizeof(small_dp[0]));
-      for (int sum = -M; sum <= M; ++sum) {
-        int w = at(i, sum);
-        if (w == -1) continue;
+      for (int sum = -VM; sum <= VM; ++sum) {
+        int &w = at(i, sum);
+        if (w == -1)
+          continue;
         for (int x = -x_bound; x <= x_bound; ++x) {
-          update(at(i - 1, sum + x * p10 - x - o * p10), w + std::abs(x) * (i - 1));
+          int new_sum = sum + x * p10 - x - o * p10;
+          if (-VM <= new_sum && new_sum <= VM) {
+            update(at(i - 1, new_sum), w + std::abs(x) * (i - 1));
+          }
         }
+        w = -1;
       }
       p10 /= 10;
     }
---
+++ main/ones/wrong.cc
@@ -0,0 +1,283 @@
+#include <bits/stdc++.h>
+
+static const int WIDTH = 18;
+static const uint64_t MAX = 1000000000000000000ULL;
+static const constexpr std::array<uint64_t, WIDTH + 1> LIMIT_LIMBS{
+    0,
+    1,
+    11,
+    111,
+    1111,
+    11111,
+    111111,
+    1111111,
+    11111111,
+    111111111,
+    1111111111,
+    11111111111,
+    111111111111,
+    1111111111111,
+    11111111111111,
+    111111111111111,
+    1111111111111111,
+    11111111111111111,
+    111111111111111111};
+
+struct BigInt {
+  BigInt(int limbs, const std::string &s) : limb(limbs) {
+    for (int i = s.size(); i--;) {
+      limb[i / WIDTH] = limb[i / WIDTH] * 10 + s[i] - '0';
+    }
+  }
+
+  int limbs() const { return limb.size(); }
+
+  uint64_t operator[](int i) const { return limb[i]; }
+
+  template <bool tracking = true> void add_one() {
+    int i = 0;
+    while (i < limbs()) {
+      if (limb[i] < MAX - 1) {
+        limb[i]++;
+        if (tracking) {
+          track();
+        }
+        return;
+      }
+      limb[i++] = 0;
+    }
+    assert(i < limbs());
+  }
+
+  void sub_one() {
+    int i = 0;
+    while (i < limbs()) {
+      if (limb[i] > 0) {
+        limb[i]--;
+        return;
+      }
+      limb[i++] = MAX - 1;
+    }
+    assert(i < limbs());
+  }
+
+  bool operator<=(const BigInt &other) const {
+    for (int i = limbs(); i--;) {
+      if (limb[i] != other[i]) {
+        return limb[i] < other[i];
+      }
+    }
+    return true;
+  }
+
+  BigInt &operator-=(const BigInt &one) {
+    int borrow = 0;
+    for (int i = 0; i < limbs(); ++i) {
+      if (limb[i] - borrow >= one[i]) {
+        limb[i] -= borrow + one[i];
+        borrow = 0;
+      } else {
+        limb[i] += MAX - borrow - one[i];
+        borrow = 1;
+      }
+    }
+    return *this;
+  }
+
+  // mod by other, and return the quotient
+  int mod(const BigInt &other) {
+    int q = 0;
+    while (other <= *this) {
+      q++;
+      *this -= other;
+    }
+    return q;
+  }
+
+  uint64_t limit_limb(int i) const {
+    int full = limit / WIDTH;
+    if (i < full) {
+      return LIMIT_LIMBS[WIDTH];
+    }
+    if (i == full) {
+      return LIMIT_LIMBS[limit % WIDTH];
+    }
+    return 0;
+  }
+
+  void track() {
+    while (tracker > 0 && limb[tracker - 1] == limit_limb(tracker - 1)) {
+      tracker--;
+    }
+  }
+
+  void set_limit(int limit_) {
+    limit = limit_;
+    tracker = limbs();
+    track();
+  }
+
+  // std::string to_string() const {
+  //   std::vector<char> buffer(WIDTH * limbs() + 1);
+  //   for (int i = limbs(); i--;) {
+  //     sprintf(buffer.data(), "%018lu", limb[i]);
+  //   }
+  //   return buffer.data();
+  // }
+
+  void set_zero() {
+    std::fill(limb.begin(), limb.end(), 0);
+    set_limit(limit);
+  }
+
+  bool limit_reached() const { return tracker == 0; }
+
+  std::vector<uint64_t> limb;
+  int limit, tracker;
+};
+
+bool operator==(const BigInt &a, const BigInt &b) {
+  for (int i = 0; i < a.limbs(); ++i) {
+    if (a[i] != b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+struct Segment {
+  Segment(const BigInt &start_) : start(start_) {}
+
+  void add(int v) { values.push_back(v); }
+
+  int size() const { return values.size(); }
+
+  BigInt start;
+  std::vector<int> values;
+};
+
+std::vector<Segment> merge(const BigInt &zero,
+                           const std::vector<Segment> &segs) {
+  std::vector<Segment> result;
+  int n = segs.size();
+  std::vector<int> cur(n, -1);
+  BigInt start = zero;
+  Segment seg{zero};
+  while (true) {
+    int v = INT_MAX;
+    for (int i = 0; i < n; ++i) {
+      if (segs[i].start == start) {
+        cur[i] = 0;
+      }
+      if (0 <= cur[i] && cur[i] < segs[i].size()) {
+        v = std::min(v, segs[i].values[cur[i]++]);
+      }
+    }
+    if (v == INT_MAX) {
+      if (seg.size()) {
+        result.push_back(seg);
+      }
+      int i = 0;
+      while (i < n && ~cur[i]) {
+        i++;
+      }
+      if (i == n) {
+        return result;
+      }
+      start = segs[i].start;
+      for (int j = i; j < n; ++j) {
+        if (cur[j] == -1 && segs[j].start <= start) {
+          start = segs[j].start;
+        }
+      }
+      seg = Segment{start};
+    } else {
+      seg.add(v);
+      start.add_one<false>();
+    }
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::string s;
+  while (std::cin >> s) {
+    std::reverse(s.begin(), s.end());
+    int digits = (s.size() + 1 + WIDTH - 1) / WIDTH;
+    BigInt zero{digits, ""};
+    std::vector<Segment> psegs, nsegs;
+    {
+      Segment seg{BigInt{digits, s}};
+      seg.add(0);
+      psegs.push_back(seg);
+    }
+    BigInt pre_one{digits, ""};
+    for (int length = s.size() + 1; length > 0; --length) {
+      std::vector<Segment> new_psegs, new_nsegs;
+      BigInt cur_one{digits, std::string(length, '1')};
+      for (auto &&seg : psegs) {
+        BigInt r = seg.start;
+        int k = r.mod(cur_one);
+        Segment new_pseg{r}, new_nseg{r};
+        r.set_limit(length);
+        r.add_one();
+        for (int v : seg.values) {
+          new_pseg.add(v + k * length);
+          new_nseg.add(v + (k + 1) * length);
+          if (length < 10 && r.limit_reached()) {
+            new_psegs.push_back(new_pseg);
+            new_nsegs.push_back(new_nseg);
+            k++;
+            r.set_zero();
+            new_pseg = new_nseg = Segment{r};
+            r.add_one();
+          } else {
+            r.add_one();
+          }
+        }
+        if (new_pseg.size()) {
+          new_psegs.push_back(new_pseg);
+          new_nsegs.push_back(new_nseg);
+        }
+      }
+      for (auto &&seg : nsegs) {
+        BigInt r = seg.start;
+        int k;
+        if (r == zero) {
+          r = cur_one;
+          r.sub_one();
+          k = 10;
+        } else {
+          r.sub_one();
+          k = 9 - r.mod(cur_one);
+        }
+        Segment new_pseg{r}, new_nseg{r};
+        r.set_limit(length);
+        r.add_one();
+        for (int v : seg.values) {
+          new_nseg.add(v + k * length);
+          new_pseg.add(v + (k + 1) * length);
+          if (length < 10 && r.limit_reached()) {
+            new_psegs.push_back(new_pseg);
+            new_nsegs.push_back(new_nseg);
+            k--;
+            r.set_zero();
+            new_pseg = new_nseg = Segment{r};
+            r.add_one();
+          } else {
+            r.add_one();
+          }
+        }
+        if (new_pseg.size()) {
+          new_psegs.push_back(new_pseg);
+          new_nsegs.push_back(new_nseg);
+        }
+      }
+      psegs = merge(zero, new_psegs);
+      nsegs = merge(zero, new_nsegs);
+    }
+    assert(psegs.size());
+    assert(psegs[0].start == zero);
+    printf("%d\n", psegs[0].values[0]);
+  }
+}
