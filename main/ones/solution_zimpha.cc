ea9dd0e6ebc426737b9e7b65db82fe623eabfa4d
Xi Lin <zimpha@gmail.com>
2021-05-25 23:04:51 +0800

    fix constant

--- main/ones/solution_zimpha.cc
+++ main/ones/solution_zimpha.cc
@@ -6,14 +6,16 @@
 
 using pii = std::pair<int, int>;
 
-const int N = 1e4 + 10, M = 1e6 + 10;
+const int N = 5000 + 10, M = 1e6 + 10;
+const int XM = N * 6;
+const int VM = XM + 1e5 + 10;
 
 char s[N];
 int _table[M * 2 + 1], pw[10];
 int *table = _table + M;
 
-int dp[2][11][50001];
-int small_dp[2][M * 2 + 1];
+int dp[2][3][XM * 2 + 1];
+int small_dp[2][VM * 2 + 1];
 
 void prepare() {
   pw[0] = 1;
@@ -43,9 +45,9 @@ void prepare() {
   }
 }
 
-int &at(int i, int carry, int sum) { return dp[i & 1][carry + 5][sum + 25000]; }
+int &at(int i, int carry, int sum) { return dp[i & 1][carry + 1][sum + XM]; }
 
-int &at(int i, int sum) { return small_dp[i & 1][sum + M]; }
+int &at(int i, int sum) { return small_dp[i & 1][sum + VM]; }
 
 void update(int &x, int y) {
   if (x == -1 || x > y)
@@ -77,22 +79,20 @@ int main() {
     memset(dp, -1, sizeof(dp));
     at(n + 1, 0, 0) = 0;
     for (int i = n + 1; i > 5; --i) {
-      memset(dp[(i - 1) & 1], -1, sizeof(dp[0]));
       int o = (i - 1 < n) ? (s[i - 1] - '0') : 0;
       for (int carry = -carry_bound; carry <= carry_bound; ++carry) {
         for (int sum = -x_bound * n; sum <= n * x_bound; ++sum) {
-          int w = at(i, carry, sum);
+          int &w = at(i, carry, sum);
           if (w == -1)
             continue;
           for (int x = -x_bound; x <= x_bound; ++x) {
             int new_carry = carry * 10 + x - o;
             int new_sum = x + sum;
-            // printf("i=%d carry=%d sum=%d new_carry=%d new_sum=%d\n", i,
-            // carry, sum, new_carry, new_sum);
             if (-carry_bound <= new_carry && new_carry <= carry_bound) {
               update(at(i - 1, new_carry, new_sum), w + std::abs(x) * (i - 1));
             }
           }
+          w = -1;
         }
       }
     }
@@ -102,23 +102,23 @@ int main() {
         int w = at(5, carry, sum);
         if (w == -1)
           continue;
-        // printf("carry=%d sum=%d %d %d\n", carry, sum, carry * 100000 - sum,
-        // w);
         update(at(5, carry * 100000 - sum), w);
       }
     }
     int p10 = 10000;
     for (int i = 5; i >= 1; --i) {
       int o = s[i - 1] - '0';
-      memset(small_dp[(i - 1) & 1], -1, sizeof(small_dp[0]));
-      for (int sum = -M; sum <= M; ++sum) {
-        int w = at(i, sum);
+      for (int sum = -VM; sum <= VM; ++sum) {
+        int &w = at(i, sum);
         if (w == -1)
           continue;
         for (int x = -x_bound; x <= x_bound; ++x) {
-          update(at(i - 1, sum + x * p10 - x - o * p10),
-                 w + std::abs(x) * (i - 1));
+          int new_sum = sum + x * p10 - x - o * p10;
+          if (-VM <= new_sum && new_sum <= VM) {
+            update(at(i - 1, new_sum), w + std::abs(x) * (i - 1));
+          }
         }
+        w = -1;
       }
       p10 /= 10;
     }
