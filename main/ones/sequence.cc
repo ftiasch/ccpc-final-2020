c9be672da0eec1092196257b82d0406e61781aaf
Sd-Invol <sd.invol@gmail.com>
2021-05-26 00:41:37 +0800

    Revert "Merge branch 'main' of https://github.com/ftiasch/ccpc-final-2020 into main"
    This reverts commit ea6807071f754a2d9f54446497371e91c9e3df67, reversing
    changes made to ebacc1228d819c50737ca2bf06e22a950cdaa87f.

--- main/ones/sequence.cc
+++ main/ones/sequence.cc
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
   registerGen(argc, argv, 1);
   int T = std::atoi(argv[1]);
-  for (int n : rnd.perm(T, 1)) {
+  for (int n = 1; n <= T; ++n) {
     printf("%d\n", n);
   }
 }
