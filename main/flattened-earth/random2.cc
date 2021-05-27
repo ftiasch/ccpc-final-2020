ebed5a5647f8367387f883a84f8be8824225a9d5
Xiaoxu Guo <ftiasch0@gmail.com>
2021-05-27 13:26:01 +0800

    fixed tests

--- main/flattened-earth/random2.cc
+++ main/flattened-earth/random2.cc
@@ -3,26 +3,39 @@
 int main(int argc, char *argv[]) {
   registerGen(argc, argv, 1);
   int T = opt<int>("T");
+  const bool cross = has_opt("allow_cross");
   while (T--) {
     const int n = has_opt("n") ? opt<int>("n") : rnd.next(2, opt<int>("maxn"));
     const int maxlen =
         has_opt("maxlen") ? std::min(n / 2, opt<int>("maxlen")) : n / 2;
     std::vector<std::pair<int, int>> intervals;
-    for (int i = 0; i < n / 2; ++i) {
-      int j = std::min(i + rnd.next(1, maxlen), n - 1);
-      intervals.emplace_back(i, j);
+    {
+      int right = 0;
+      for (int i = 0; i < n / 2; ++i) {
+        if (i == right) {
+          int j = std::min(i + rnd.next(1, maxlen), n - 1);
+          intervals.emplace_back(i, j);
+          right = j;
+        }
+      }
     }
-    for (int i = n / 2 + 1; i < n; ++i) {
-      int j = std::max(i - rnd.next(1, maxlen), 0);
-      intervals.emplace_back(i, j);
+    {
+      int left = n - 1;
+      for (int i = n - 1; i > n / 2; --i) {
+        if (i == left) {
+          int j = std::max(i - rnd.next(1, maxlen), 0);
+          intervals.emplace_back(i, j);
+          left = j;
+        }
+      }
     }
     const int m = has_opt("m") ? opt<int>("m")
                                : rnd.next(static_cast<int>(intervals.size()),
                                           opt<int>("maxn"));
     while (static_cast<int>(intervals.size()) < m) {
       int len = rnd.next(0, maxlen);
-      int i = rnd.next(len, n - 1);
-      intervals.emplace_back(i, i + len);
+      int i = cross ? rnd.next(0, n - 1) : rnd.next(len, n - 1);
+      intervals.emplace_back((i + n - len) % n, i);
     }
     shuffle(intervals.begin(), intervals.end());
     const int offset = rnd.next(0, n - 1);
