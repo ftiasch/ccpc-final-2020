ce218e6df756078e062258719bf123a898191819
Xiaoxu Guo <ftiasch0@gmail.com>
2021-05-29 15:02:47 +0800

    [WIP]

--- main/ones/test.py
+++ main/ones/test.py
@@ -103,114 +103,125 @@ def solve0(n):
     neg = []
     for length in range(len(str(n)) + 1, 0, -1):
         pos, neg = trans0(length, pos, neg)
+        # if len(pos) == 1:
+        #     print(pos[0].start, len(pos[0].values))
+        # else:
+        #     print(pos[1].start, len(pos[0].values) + len(pos[1].values))
+        # print(pos)
     assert pos
     assert pos[0].start == 0
     return pos[0].values[0]
 
 
-DP_MAX = 10 ** 9
+# DP_MAX = 10 ** 9
+#
+#
+# def trans_large(length, start, positives, negatives):
+#     one = make_one(length)
+#     if start is not None:
+#         assert len(positives) + 1 < one
+#         r = start % one
+#         k = start // one
+#         one_minus_r = min(one - r, len(positives) + 1)
+#         if r <= 1:
+#             assert len(positives) < one_minus_r
+#             # becomes prefix
+#         elif len(positives) == one_minus_r:
+#             # becomes suffix
+#             pass
+#         elif len(positives) > one_minus_r:
+#             # becomes prefix + suffix
+#             pass
+#         else:
+#             size = len(positives) + 1
+#             kpositives = [DP_MAX] * size
+#             knegatives = [DP_MAX] * size
+#             for i, v in enumerate(positives):
+#                 kpositives[i + 1] = min(kpositives[i + 1], v + k * length)
+#                 knegatives[i + 1] = min(knegatives[i + 1],
+#                                         v + (k + 1) * length)
+#             for i, v in enumerate(negatives):
+#                 knegatives[i] = min(knegatives[i], v + (9 - k) * length)
+#                 kpositives[i] = min(kpositives[i], v + (10 - k) * length)
+#             return r - 1, kpositives, knegatives
+#     else:
+#         pprefix, psuffix = positives
+#         nprefix, nsuffix = negatives
+#         kprefix_size = max(len(pprefix), 1)
+#         kpprefix = [DP_MAX] * kprefix_size
+#         knprefix = [DP_MAX] * kprefix_size
+#         ksuffix_size = max(len(nsuffix), 1)
+#         kpsuffix = [DP_MAX] * ksuffix_size
+#         knsuffix = [DP_MAX] * ksuffix_size
+#         assert kprefix_size + ksuffix_size < one
+#         for i, v in enumerate(pprefix):
+#             kpprefix[i] = min(kpprefix[i], v)
+#             knprefix[i] = min(knprefix[i], v + length)
+#         if psuffix:
+#             kpprefix[0] = min(kpprefix[0], psuffix[0] + 10 * length)
+#             knprefix[0] = min(knprefix[0], psuffix[0] + 11 * length)
+#             for i, v in enumerate(pprefix):
+#                 if i > 0:
+#                     kpsuffix[i - 1] = min(kpsuffix[i - 1], v + 9 * length)
+#                     knsuffix[i - 1] = min(knsuffix[i - 1], v + 10 * length)
+#         for i, v in enumerate(nsuffix):
+#             knsuffix[i] = min(knsuffix[i], v)
+#             kpsuffix[i] = min(kpsuffix[i], v + length)
+#         if nprefix:
+#             knsuffix[0] = min(knsuffix[0], v + 10 * length)
+#             kpsuffix[0] = min(kpsuffix[0], v + 11 * length)
+#             for i, v in enumerate(nprefix):
+#                 if i > 0:
+#                     knprefix[i - 1] = min(knprefix[i - 1], v + 9 * length)
+#                     kpprefix[i - 1] = min(kpprefix[i - 1], v + 10 * length)
+#         return None, (kpprefix, kpsuffix), (knprefix, knsuffix)
+#
+#
+# def solve(n):
+#     # dp_start = n
+#     # pdp = [0]
+#     # ndp = [0]
+#     length = len(str(n))
+#     one = make_one(length + 1)
+#     positives = [0]
+#     negatives = [length + 1]
+#     start = n
+#     while length > 0:
+#         trans_large(length, start, positives, negatives)
+#
+# def gen_seg0(pre_one, dp_length, r):
+#     if r + dp_length <= pre_one:
+#         values = [random.randint(1, 100) for i in range(dp_length)]
+#         return [Segment0(r, values)]
+#     prefix_length = pre_one - r
+#     suffix_length = dp_length - prefix_length
+#     prefix_values = [random.randint(1, 100) for i in range(prefix_length)]
+#     suffix_values = [random.randint(1, 100) for i in range(suffix_length)]
+#     return [Segment0(0, prefix_values), Segment0(r, suffix_values)]
 
-
-def trans_large(length, start, positives, negatives):
-    one = make_one(length)
-    if start is not None:
-        assert len(positives) + 1 < one
-        r = start % one
-        k = start // one
-        one_minus_r = min(one - r, len(positives) + 1)
-        if r <= 1:
-            assert len(positives) < one_minus_r
-            # becomes prefix
-        elif len(positives) == one_minus_r:
-            # becomes suffix
-            pass
-        elif len(positives) > one_minus_r:
-            # becomes prefix + suffix
-            pass
+def solve0_start(n):
+    pos = [Segment0(n, [0])]
+    neg = []
+    starts = []
+    for length in range(len(str(n)) + 1, 0, -1):
+        pos, neg = trans0(length, pos, neg)
+        if len(pos) == 1:
+            starts.append(pos[0].start)
+            # print(pos[0].start, len(pos[0].values))
         else:
-            size = len(positives) + 1
-            kpositives = [DP_MAX] * size
-            knegatives = [DP_MAX] * size
-            for i, v in enumerate(positives):
-                kpositives[i + 1] = min(kpositives[i + 1], v + k * length)
-                knegatives[i + 1] = min(knegatives[i + 1],
-                                        v + (k + 1) * length)
-            for i, v in enumerate(negatives):
-                knegatives[i] = min(knegatives[i], v + (9 - k) * length)
-                kpositives[i] = min(kpositives[i], v + (10 - k) * length)
-            return r - 1, kpositives, knegatives
-    else:
-        pprefix, psuffix = positives
-        nprefix, nsuffix = negatives
-        kprefix_size = max(len(pprefix), 1)
-        kpprefix = [DP_MAX] * kprefix_size
-        knprefix = [DP_MAX] * kprefix_size
-        ksuffix_size = max(len(nsuffix), 1)
-        kpsuffix = [DP_MAX] * ksuffix_size
-        knsuffix = [DP_MAX] * ksuffix_size
-        assert kprefix_size + ksuffix_size < one
-        for i, v in enumerate(pprefix):
-            kpprefix[i] = min(kpprefix[i], v)
-            knprefix[i] = min(knprefix[i], v + length)
-        if psuffix:
-            kpprefix[0] = min(kpprefix[0], psuffix[0] + 10 * length)
-            knprefix[0] = min(knprefix[0], psuffix[0] + 11 * length)
-            for i, v in enumerate(pprefix):
-                if i > 0:
-                    kpsuffix[i - 1] = min(kpsuffix[i - 1], v + 9 * length)
-                    knsuffix[i - 1] = min(knsuffix[i - 1], v + 10 * length)
-        for i, v in enumerate(nsuffix):
-            knsuffix[i] = min(knsuffix[i], v)
-            kpsuffix[i] = min(kpsuffix[i], v + length)
-        if nprefix:
-            knsuffix[0] = min(knsuffix[0], v + 10 * length)
-            kpsuffix[0] = min(kpsuffix[0], v + 11 * length)
-            for i, v in enumerate(nprefix):
-                if i > 0:
-                    knprefix[i - 1] = min(knprefix[i - 1], v + 9 * length)
-                    kpprefix[i - 1] = min(kpprefix[i - 1], v + 10 * length)
-        return None, (kpprefix, kpsuffix), (knprefix, knsuffix)
+            starts.append(pos[1].start)
+    return starts
 
-
-def solve(n):
-    # dp_start = n
-    # pdp = [0]
-    # ndp = [0]
-    length = len(str(n))
-    one = make_one(length + 1)
-    positives = [0]
-    negatives = [length + 1]
-    start = n
-    while length > 0:
-        trans_large(length, start, positives, negatives)
-
-def gen_seg0(pre_one, dp_length, r):
-    if r + dp_length <= pre_one:
-        values = [random.randint(1, 100) for i in range(dp_length)]
-        return [Segment0(r, values)]
-    prefix_length = pre_one - r
-    suffix_length = dp_length - prefix_length
-    prefix_values = [random.randint(1, 100) for i in range(prefix_length)]
-    suffix_values = [random.randint(1, 100) for i in range(suffix_length)]
-    return [Segment0(0, prefix_values), Segment0(r, suffix_values)]
-
-
+def solve_start(n):
+    r = n + 1
+    starts = []
+    for length in range(len(str(n)) + 1, 0, -1):
+        one = make_one(length)
+        r = (r + one - 1) % one
+        starts.append(r)
+    return starts
 
 
 if __name__ == '__main__':
-    random.seed(0)
-    length = 2
-    pre_one = make_one(length + 1)
-    cur_one = make_one(length)
-    dp_length = random.randint(1, 9)
-    r = random.randint(0, pre_one - 1)
-
-    pos0 = gen_seg0(pre_one, dp_length, r)
-    neg0 = gen_seg0(pre_one, dp_length, r)
-    pos0, neg0 = trans0(length, pos0, neg0)
-
-    # print(solve0(n))
-    # solve(n)
-    # for n in range(1, 10000):
-    #     print('n=', n)
-    #     assert solve0(n) == solve(n)
+    for n in range(1, 10 ** 5):
+        assert solve0_start(n) == solve_start(n)
